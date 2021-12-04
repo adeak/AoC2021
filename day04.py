704a04c9f003d5ec461a1b37b67af266ded0ef1e
adeak <adeak@users.noreply.github.com>
2021-12-04 10:59:04 +0100

    Day 04 slightly improved

--- day04.py
+++ day04.py
@@ -1,12 +1,10 @@
 import numpy as np
 
 def day04(inp):
-    part1 = part2 = None
     blocks = inp.strip().split('\n\n')
     nums = np.fromstring(blocks[0], sep=',', dtype=int)
     boards = np.array([np.fromstring(block, sep=' ', dtype=int).reshape(5, 5) for block in blocks[1:]])
 
-    num_win = None
     last_winners = set()
     for num in nums:
         # set drawn number to -1
@@ -15,13 +13,12 @@ def day04(inp):
         # check if there's a winner
         hits = boards == -1
         wins = hits.all(1).any(-1) | hits.all(-1).any(-1)
-        if wins.sum() == 1 and num_win is None:
-            # part 1
-            num_win = num
+        if wins.sum() == 1 and not last_winners:
+            # part 1: first winner
             winner = boards[wins.argmax(), ...]
-            part1 = winner[winner != -1].sum() * num_win
-        if wins.sum() == wins.size and len(last_winners) == wins.size - 1:
-            # part 2
+            part1 = winner[winner != -1].sum() * num
+        if wins.sum() == wins.size == len(last_winners) + 1:
+            # part 2: last winner
             winner_index = (set(range(wins.size)) - last_winners).pop()
             winner = boards[winner_index, ...]
             part2 = winner[winner != -1].sum() * num
