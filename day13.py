1769fe5da72ffa05e8a7d09ef308d7c0f09effea
adeak <adeak@users.noreply.github.com>
2021-12-13 12:07:19 +0100

    Day 13 cleaned up

--- day13.py
+++ day13.py
@@ -9,6 +9,8 @@ def day13(inp):
         inds.append(list(map(int, row.split(','))))
     inds = np.array(inds)  # shape (ndots, 2)
 
+    # keep the array oriented the same as the input example
+    # (i.e. x and y are reversed)
     shape = (inds.max(0) + 1)[::-1]
     sheet = np.zeros(shape=shape, dtype=bool)
     sheet[inds[:, 1], inds[:, 0]] = True
@@ -17,34 +19,33 @@ def day13(inp):
         info = row.split()[-1]
         dir, center = info.split('=')
         center = int(center)
-        if dir == 'y':
-            assert not sheet[center, :].any()
-            len_1 = center
-            len_2 = sheet.shape[0] - center - 1
-            foldlen = min([len_1, len_2])
-            if len_2 < len_1:
-                sheet[center-1:center-1-foldlen:-1, :] |= sheet[center+1:, :]
-                sheet = sheet[:center, :]
-            else:
-                sheet[center+1:center+1+foldlen, :] |= sheet[center-1::-1, :]
-                sheet = sheet[center+1:, :]
+
         if dir == 'x':
-            assert not sheet[:, center].any()
-            len_1 = center
-            len_2 = sheet.shape[1] - center - 1
-            foldlen = min([len_1, len_2])
-            if len_2 < len_1:
-                sheet[:, center-1:center-1-foldlen:-1] |= sheet[:, center+1:]
-                sheet = sheet[:, :center]
-            else:
-                sheet[:, center+1:center+1+foldlen] |= sheet[:, center-1::-1]
-                sheet = sheet[:, center+1:]
+            # just work on the transpose
+            sheet = sheet.T
+
+        assert not sheet[center, :].any()
+        len_1 = center
+        len_2 = sheet.shape[0] - center - 1
+        foldlen = min([len_1, len_2])
+        if len_2 < len_1:
+            # keep the "top" part
+            sheet[center-foldlen:center, :] |= sheet[:center:-1, :]
+            sheet = sheet[:center, :]
+        else:
+            # keep the "bottom" part
+            sheet[center+1+foldlen:center:-1, :] |= sheet[:center, :]
+            sheet = sheet[:center:-1, :]  # keep "up" up
+
+        if dir == 'x':
+            # transpose back for next fold
+            sheet = sheet.T
 
         if i == 1:
             part1 = sheet.sum()
 
     # part 2
-    print('\n'.join(''.join('#' if c else '.' for c in row) for row in sheet[::-1, ::-1]))
+    print('\n'.join(''.join('*' if c else ' ' for c in row) for row in sheet))
 
     return part1
 
