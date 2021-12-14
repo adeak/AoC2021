ac721541a1d6754080eb9fdc23dee2b018017a6a
adeak <adeak@users.noreply.github.com>
2021-12-14 14:51:12 +0100

    Day 14 both parts (rewrite)

--- day14.py
+++ day14.py
@@ -1,28 +1,50 @@
 from collections import Counter
-from operator import itemgetter
 
 def day14(inp):
-    part1 = part2 = None
     it = iter(inp.splitlines())
 
     template = next(it)
     next(it)
     rules = dict(row.split(' -> ') for row in it)
 
-    polymer = template
-    for i in range(10):
-        next_poly = [polymer[0]]
-        pairs = (polymer[ind:ind+2] for ind in range(len(polymer) - 1))
-        for pair in pairs:
-            infix = rules.get(pair, '')
-            next_poly.append(infix + pair[1])
-        polymer = ''.join(next_poly)
+    # get pair counts
+    pair_counts = Counter(template[ind:ind+2] for ind in range(len(template) - 1))
 
-    counts = Counter(polymer)
-    ranked = sorted(counts.values())
-    part1 = ranked[-1] - ranked[0]
+    # rules to generate new pairs
+    pair_rules = {pair: [pair[0] + insert, insert + pair[1]] for pair, insert in rules.items()}
 
-    return part1, part2
+    polymer_counts = pair_counts
+    for i in range(40):
+        next_counts = Counter()
+        for pair, count in polymer_counts.items():
+            if pair in pair_rules:
+                # we only have to add the two new pairs
+                for replacing_pair in pair_rules[pair]:
+                    next_counts[replacing_pair] += count
+            else:
+                # we keep the original pair
+                next_counts[pair] += count
+
+        polymer_counts = next_counts
+
+        if i == 9:
+            pair1_counts = polymer_counts
+
+    # convert pair counts to letter counts
+    parts = []
+    for counts in pair1_counts, polymer_counts:
+        letter_counts = Counter()
+        for pair, freq in counts.items():
+            # count the first letter of each pair
+            letter_counts[pair[0]] += freq
+        # edge case: last letter is missing this way
+        letter_counts[template[-1]] += 1
+
+        ranked = sorted(letter_counts.values())
+        res = ranked[-1] - ranked[0]
+        parts.append(res)
+
+    return parts
 
 
 if __name__ == "__main__":
