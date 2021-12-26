fef47ecd64566d0b0778d9cf669230659baec2fc
adeak <adeak@users.noreply.github.com>
2021-12-26 13:43:39 +0100

    Day 23, A* with algo rewritten from scratch, fast and correct

--- day23.py
+++ day23.py
@@ -1,6 +1,5 @@
 from collections import defaultdict
-from operator import itemgetter
-from heapq import heappush, heappop, heapify
+from heapq import heappush, heappop
 
 def day23(inp, part2=False):
     type_costs = dict(zip('ABCD', [1, 10, 100, 1000]))
@@ -49,146 +48,123 @@ def day23(inp, part2=False):
 
     # try A* searching for the cheapest path
     # a single state is a dict of (i, j) -> type items, with length 8 (or 16)
+    # (well, converted to a tuple of the dict items for hashability)
     max_depth = max(tiles)[0]
     target = {
         (depth, 2*i + 1): kind
         for i, kind in enumerate('ABCD', 1)
         for depth in range(2, max_depth + 1)
-    }
-    rooms = {kind: index[1] for index, kind in target.items()}
-
-    # # try to give an upper bound to cost
-    # # upper bound for cost: move everyone to a further corner, then to the bottom of their rooms
-    # corners = [(1, 1), (1, 11)]
-    # cost_upper_bound = 0
-    # for pos, kind in poses.items():
-    #     goal = (max_depth, rooms[kind])
-    #     potential_costs = [
-    #         type_costs[kind]*(steps[pos, corner] + steps[corner, goal])
-    #         for corner in corners
-    #     ]
-    #     cost_upper_bound += max(potential_costs)
-    # cost_upper_bound = cost_upper_bound*2//3  # fudge factor; result seems safe from part1, and part2 testinp
-    # print(cost_upper_bound)
-
+    }  # the end state we're searching for
+    rooms = {kind: index[1] for index, kind in target.items()}  # type -> room column mapping
 
     all_costs = {tuple(sorted(poses.items())): 0}  # tuple of dict items -> cost mapping
     start = tuple(sorted(poses.items()))
-    initial_cost_bound = cost_lower_bound(start, target, type_costs, steps, rooms)
-    #all_cost_bounds = {tuple(sorted(poses.items())): initial_cost_bound}  # tuple of dict items -> cost mapping
-    #edges = {(initial_cost_bound, start)}  # set of (cost lower bound, edge state) tuples
+    initial_cost_bound = cost_lower_bound(start, target, type_costs, steps, rooms)  # lower bound for total cost
     edges = [(initial_cost_bound, start)]  # heapq of (cost lower bound, edge state) tuples
-    #edges = {start: initial_cost_bound}  # dict of positions -> cost estimate mapping
-    it = 0
     while edges:
-        # TODO DEBUG tmp FIXME:
-        it += 1
-        #edges = [(cost, poses) for poses, cost in dict((p, c) for c, p in sorted(edges)).items()]
-        #heapify(edges)
-        # assert len(edges) == len({start for cost, start in edges}), (len(edges), len({start for cost, start in edges})) # no duplicate positions
-        # take the cheapest step
-        #edge, cost_estimate = min(edges.items(), key=itemgetter(1))
         cost_estimate, edge = heappop(edges)
-        cost_now = all_costs[edge]  # actual cost of reaching "edge"
-        #print(f'Cost now: {cost_now}, {cost_estimate}...')
+        cost_now = all_costs[edge]
         edge_dict = dict(edge)
         poses = dict(edge).keys()
 
-        #print_poses(edge_dict, tiles)
-
         # explore new states accessible from this step
         empty_tiles = tiles - poses
+
+        valid_moves = []  # (pos, next_pos) tuples collected for a given state
+
+        # first check if we can move someone to their room (either from hallway or other room)
         for pos, kind in edge:
-            # consider moving this animal
+            target_col = rooms[kind]
 
-            # if this is in the right place, don't touch it
-            if pos[0] > 1 and pos[1] == rooms[kind]:
-                if pos[0] == max_depth:
-                    # we're at the bottom
+            if pos[0] > 1 and pos[1] == target_col:
+                # we're in the right room, no need/possibility to move to right place now
+                continue
+
+            # check if the room is free
+            found_valid = False
+            for next_depth in range(max_depth, 1, -1):
+                next_pos = (next_depth, target_col)
+                if edge_dict.get(next_pos, kind) != kind:
+                    # there's an impostor inside, abort
+                    break
+                if edge_dict.get(next_pos, '') == kind:
+                    # position is filled with correct kind
                     continue
-                if all(edge_dict.get((i, pos[1]), '') == kind for i in range(pos[0] + 1, max_depth + 1)):
-                    # we're on top of the right kind(s); also done
-                    continue
-            for next_pos in empty_tiles:
-                if blockers[pos, next_pos] & poses:
-                    # we can't move there due to blocker
+                if next_pos not in poses:
+                    # this is a valid occupancy, check if we can go there
+                    if blockers[pos, next_pos] & poses:
+                        # can't go there
+                        break
+                    found_valid = True
+                    break
+            if found_valid:
+                # then we should move pos -> next_pos
+                valid_moves.append((pos, next_pos))
+
+        # if there were valid moves here: don't try room -> hallway moves (heuristic)
+        if not valid_moves:
+            # gather all room -> hallway moves
+            targets = {tile for tile in empty_tiles if tile[0] == 1}
+
+            for room_col in 3, 5, 7, 9:
+                # find topmost occupant
+                for depth in range(2, max_depth + 1):
+                    pos = (depth, room_col)
+                    if pos in poses:
+                        break
+                else:
+                    # room is empty
                     continue
 
-                # animals in hallways can only move to their destination
-                if pos[0] == 1:
-                    if next_pos[0] == 1 or next_pos[1] != rooms[kind]:
-                        # this is not our room
+                # make sure we're not trying to move someone already in the right place
+                if all(edge_dict.get((d, room_col), '') == target[(d, room_col)] for d in range(depth, max_depth + 1)):
+                    continue
+
+                # move this animal to each possible hallway tile
+                for hallway_tile in targets:
+                    bounds = sorted([pos[1], rooms[edge_dict[pos]]])
+                    if bounds[0] < hallway_tile[1] < bounds[1]:
+                        # don't stop mid-way between starting room and target room
                         continue
-                    if any(edge_dict.get((i, next_pos[1]), kind) != kind for i in range(next_pos[0] + 1, max_depth + 1)):
-                        # there are impostors inside
-                        continue
-                elif next_pos[0] != 1:
-                    # we also have to enter the hallway when moving from one room to another
-                    if next_pos[1] != pos[1] and next_pos[0] < max_depth:
-                        if any(edge_dict.get((i, next_pos[1]), kind) != kind for i in range(next_pos[0] + 1, max_depth + 1)):
-                            # there are impostors inside
-                            continue
-                
-                # _now_ we have a potential move from pos to next_pos
-                new_cost = cost_now + type_costs[kind]*steps[pos, next_pos]
-                next_poses = {
-                    tmp_pos if tmp_pos != pos else next_pos: tmp_type
-                    for tmp_pos, tmp_type in edge
-                }
-                hashable_next_poses = tuple(sorted(next_poses.items()))
-                old_cost = all_costs.get(hashable_next_poses, float('inf'))
-                if new_cost >= old_cost:
-                    # we were here before and now it's not better at all
-                    continue
-                new_cost_bound = cost_lower_bound(next_poses, target, type_costs, steps, rooms)
-                #if hashable_next_poses in all_cost_bounds:
-                #    if all_cost_bounds[hashable_next_poses] < new_cost_bound:
-                #        # we were here before and better
-                #        # (probably won't happen)
-                #        assert False
-                #        continue
-                #    assert False, (edges, hashable_next_poses)
-                # TODO: revisit or remove this:
-                # if new_cost >= cost_upper_bound:
-                #     # this has to be a bad path
-                #     continue
+                    if not blockers[pos, hallway_tile] & poses:
+                        # the way is free
+                        valid_moves.append((pos, hallway_tile))
 
-                ## DEBUG TODO REMOVE: plot move
-                #print_poses(poses, tiles)
-                #print_poses(next_poses, tiles)
-                #print()
-                #input()
+        # now add all the valid moves (unless it's worse than before)
+        for pos, next_pos in valid_moves:
+            kind = edge_dict[pos]
+            new_cost = cost_now + type_costs[kind]*steps[pos, next_pos]
+            next_poses = {
+                tmp_pos if tmp_pos != pos else next_pos: tmp_type
+                for tmp_pos, tmp_type in edge
+            }
+            hashable_next_poses = tuple(sorted(next_poses.items()))
+            old_cost = all_costs.get(hashable_next_poses, float('inf'))
+            if new_cost >= old_cost:
+                # we were here before and now it's not better at all
+                continue
+            new_cost_bound = cost_lower_bound(next_poses, target, type_costs, steps, rooms)
 
-                # _now_ it's worth moving from pos to next_pos
-                if next_poses == target:
-                    #print(f'Best cost for target: {cost}')
-                    print(it, 'iterations')
-                    return new_cost
-                all_costs[hashable_next_poses] = new_cost
-                #all_cost_bounds[hashable_next_poses] = new_cost_bound
-                cost_estimate = new_cost + new_cost_bound
-                heappush(edges, (cost_estimate, hashable_next_poses))
-        
-        if not it % 10_000:
-            # prune duplicate positions in the heap (slow but saves memory)
-            before = len(edges)
-            edges = [(cost, poses) for poses, cost in dict((p, c) for c, p in sorted(edges)).items()]
-            after = len(edges)
-            print(f'pruning... {before} -> {after}')
-            #print(f'cost range: {min(edges)[0]} -> {max(edges)[0]}')
-            heapify(edges)
+            # _now_ it's worth moving from pos to next_pos
+            if next_poses == target:
+                # we're done
+                return new_cost
 
-        # # heuristic: prune 10% costliest edges...
-        # # (20% works for example, not for real data)
-        # costs = sorted((all_costs[edge], edge) for edge in edges)
-        # n_drop = len(costs)*10//100
-        # #print([item[0] for item in costs]); input()
-        # edges = {item[1] for item in costs[:-n_drop]}
+            # otherwise add this state to the edges
+            cost_estimate = new_cost + new_cost_bound
+            all_costs[hashable_next_poses] = new_cost
+            heappush(edges, (cost_estimate, hashable_next_poses))
 
 
 def print_poses(poses, tiles):
+    """
+    Quick and dirty board printing.
+
+    This takes a dict of position -> kind items, and a set of
+    valid tiles. Walls and invalid tiles are not printed.
+    """
+    import numpy as np  # out of laziness
     x_size = max(tiles)[0] + 1
-    import numpy as np
     arr = np.full((x_size, 13), fill_value=' ')
     for one_pos, kind in poses.items():
         arr[one_pos] = kind
@@ -247,20 +223,6 @@ def cost_lower_bound(hashable_state, target, type_costs, steps, rooms):
 
 if __name__ == "__main__":
     testinp = open('day23.testinp').read()
-    #print(day23(testinp), day23(testinp, part2=True))
+    print(day23(testinp), day23(testinp, part2=True))
     inp = open('day23.inp').read()
-    #print(day23(inp), day23(inp, part2=True))
-
-    #print(day23(testinp, part2=False))
-    # 24 seconds with fixed heuristic and printing
-    # 17 seconds with fixed heuristic and no printing
-    # 11 seconds with _really_ fixed heuristic and no printing
-    # 1.3 seconds with heapq
-    print(day23(inp, part2=False))
-    # 19 minutes with buggy heuristic
-    # 6 minutes 10 seconds with fixed heuristic
-    # 6.5 seconds with heapq; duplicate edges and huge memory need
-    # 5.6 seconds with heapq; pruning duplicates every 10_000 edges
-
-    #print(day23(testinp, part2=True))
-    #print(day23(inp, part2=True))
+    print(day23(inp), day23(inp, part2=True))
